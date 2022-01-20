77af220ad4cdfb14bc331c45f96690a65badc325
Corrosivebug <92105717+Corrosivebug@users.noreply.github.com>
2022-01-20 15:11:36 +0200

    lesson_2
    из 2 симестра

---
+++ mine.js
@@ -0,0 +1,55 @@
+// let massage = prompt("Ты верищ в Бога?")
+// if (massage == "да"){
+//    alert("Ну смотри мне....")
+// }
+
+
+
+// alert(1/0)
+
+
+
+// alert("число"/2)
+
+// alert("2"+2)
+
+
+// alert("2"-2)
+
+
+
+
+// alert("2"-"2")
+
+
+
+
+// let name="Max"
+// let surnmae="Maxemich"
+// let age=12
+// alert(``)
+
+
+
+
+// let name_file=true
+// alert(name_file)
+
+// let ageUser =4>1
+// alert(ageUser)
+// alert(typeof ageUser)
+
+// let a="123"
+// alert(typeof a)
+
+
+let a = Number(prompt("Введите число"))
+let b = Number(prompt("Введи ешё раз"))
+alert (a + b)
+
+
+
+
+
+
+
